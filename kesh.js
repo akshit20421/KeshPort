0868477f13b3ca81d8fa47199ddeef50cd992ab5
{Akshit} <{akshitg2042@gmail.com}>
2022-07-14 14:34:31 +0530

    complete website

--- kesh.js
+++ kesh.js
@@ -2,9 +2,11 @@
 function changeColor(){
 
   var bg=document.querySelector(".top-container");
+  var bb=document.querySelector("nav");
   if(count%2==0){
 
-  bg.style.backgroundColor="#A1DBF1";
+  bg.style.backgroundColor="#D6D5A8";
+
 }
 
 else if(count%2!=0){
@@ -14,3 +16,11 @@ else if(count%2!=0){
 count++;
 
 }
+document.querySelector(".b1").addEventListener("click",function(){
+  window.scrollTo({
+    top: 750,
+   left: 100,
+   behavior: 'smooth'
+  })
+
+});
